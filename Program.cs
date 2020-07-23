69fcf908c00bc3a6366525428a9d60437f5f9374
David Warburton <david.warburton@gmail.com>
2020-07-22 21:20:42 -0400

    fetch events

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Amazon.CloudWatchLogs;
+using Amazon.CloudWatchLogs.Model;
 
 namespace furl
 {
@@ -6,7 +8,32 @@ namespace furl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new AmazonCloudWatchLogsClient(Amazon.RegionEndpoint.USEast2);
+            var response = client.GetLogEventsAsync(mkreq()).Result;
+            dumpevts(response);
+            while (response.NextForwardToken != null) {
+                response = client.GetLogEventsAsync(mkreq(response.NextForwardToken)).Result;
+                dumpevts(response);
+            }
+        }
+        static void dumpevts(GetLogEventsResponse r) {
+            foreach (var evt in r.Events) {
+                Console.WriteLine($"{evt.Timestamp}: ${evt.Message}");
+            }
+        }
+        static GetLogEventsRequest mkreq(string tok = null) {
+            var r = new GetLogEventsRequest {
+                LogStreamName = Environment.GetEnvironmentVariable("FURL_LOG_STREAM"),
+                LogGroupName = Environment.GetEnvironmentVariable("FURL_LOG_GROUP"),
+                Limit = 5000
+            };
+            if (tok != null) {
+                r.NextToken = tok;
+            }
+            else {
+                r.StartFromHead = true;
+            }
+            return r;
         }
     }
 }
