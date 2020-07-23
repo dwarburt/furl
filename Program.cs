b2bfa1424d09d66b64cb74e339b75a7c33a114b9
David Warburton <david.warburton@gmail.com>
2020-07-22 21:16:08 -0400

    dotnet new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace furl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
